26d774a0405709887fddee631d4fa38923037adb
SeanDaBlack <seandablack@gmail.com>
2025-05-05 16:55:13 -0700

    dumb

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from faker import Faker
-import keyboard, random
+import random
 
 fake = Faker()
 
