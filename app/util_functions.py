3e78f0af1d39b6c26a3ea0087d407ca39a78aad2
SeanDaBlack <seandablack@gmail.com>
2025-05-06 08:36:29 -0700

    final?

--- app/util_functions.py
+++ app/util_functions.py
@@ -21,7 +21,7 @@ def start_driver() -> webdriver.Chrome:
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+    # options.add_argument('--disable-dev-shm-usage')
     # options.add_experimental_option("debuggerAddress", "127.0.0.1:9014")
     options.add_argument("start-maximized")
     # options.add_argument("--disable-gpu")
